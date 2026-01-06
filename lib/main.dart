1da5b87db1b3aafe3a624b22b62d2dbb2252e7a7
Official-turttle <rm.haraan@gmail.com>
2026-01-06 12:46:39 +0800

    init firebase to the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 // import 'login_screen.dart';
 import 'home_screen.dart';
 // import 'edit_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  try {
+    await Firebase.initializeApp(); // Initialize Firebase
+    print("Firebase Initialized Successfully");
+  } catch (e) {
+    print("Error initializing Firebase: $e");
+  }
+
   runApp(const App());
 }
 
